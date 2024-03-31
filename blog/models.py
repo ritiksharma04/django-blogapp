8d5d3963df90b69080117c20d03b58a78c0bd9c1
ritiksharma04 <rritiksharma04@gmail.com>
2024-04-01 00:24:43 +0530

    created db

--- blog/models.py
+++ blog/models.py
@@ -17,4 +17,5 @@ class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE) #2nd argument for deleting comment when post deleted
+
